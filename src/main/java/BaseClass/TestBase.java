3d57502d8bb58108a15a927604c00c6992be6da2
bisha <bishalshah991@gmail.com>
2023-07-11 01:45:52 +0545

    Change base cass and testng

--- src/main/java/BaseClass/TestBase.java
+++ src/main/java/BaseClass/TestBase.java
@@ -80,9 +80,8 @@ public class TestBase {
         switch (browser.toLowerCase(Locale.ROOT))
         {
             case "chrome":
-                ChromeOptions ops = new ChromeOptions();
-                ops.addArguments("--remote-allow-origins=*");
-                driver = new ChromeDriver(ops);
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
                 break;
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
