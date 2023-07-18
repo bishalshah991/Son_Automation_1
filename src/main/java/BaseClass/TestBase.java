19f0fde664fdfc7f561fc71c3a4f3bdb53577102
bishalshah991 <45026119+bishalshah991@users.noreply.github.com>
2023-07-19 03:37:44 +0545

    Update TestBase.java

--- src/main/java/BaseClass/TestBase.java
+++ src/main/java/BaseClass/TestBase.java
@@ -81,7 +81,7 @@ public class TestBase {
         switch (browser.toLowerCase(Locale.ROOT))
         {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+                System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
                 ChromeOptions options=new ChromeOptions();
                 options.addArguments("--headless");
                 options.addArguments("--no-sandbox");
